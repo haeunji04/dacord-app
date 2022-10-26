2776fbb5f0351a78b99d688cc7af60c3bb493aca
haeunji04 <ppippu12@gmail.com>
2022-10-26 23:17:50 +0900

    first commit

---
+++ src/main/java/com/study/dacord/DacordApplication.java
@@ -0,0 +1,13 @@
+package com.study.dacord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DacordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DacordApplication.class, args);
+	}
+
+}
