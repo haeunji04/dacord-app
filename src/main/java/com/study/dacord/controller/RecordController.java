7fc913f5dabce398291a5f98f0b53c276cc4be70
haeunji04 <ppippu12@gmail.com>
2022-11-08 23:37:13 +0900

    feat: 게시글 내용 수정 기능 추가, 입력값 validation 추가

--- src/main/java/com/study/dacord/controller/RecordController.java
+++ src/main/java/com/study/dacord/controller/RecordController.java
@@ -2,13 +2,16 @@ package com.study.dacord.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.study.dacord.model.Record;
 import com.study.dacord.repository.RecordRepository;
@@ -29,13 +32,25 @@ public class RecordController {
 	}
 	
 	@GetMapping("/form")
-	public String form(Model model) {
-		model.addAttribute("record", new Record());
+	public String form(Model model, @RequestParam(required = false) Long id) {
+		
+		if(id == null) {
+			model.addAttribute("record", new Record());			
+		} else {
+			Record record = recordRepository.findById(id).orElse(null);
+			model.addAttribute("record", record);
+		}
+		
 		return "record/form";
 	}
 	
 	@PostMapping("/form")
-	public String formSubmit(@ModelAttribute Record record) {
+	public String formSubmit(@Valid Record record, BindingResult bindingResult) {
+		
+		if (bindingResult.hasErrors()) {
+			return "record/form";
+		}
+		
 		recordRepository.save(record);
 		return "redirect:/record/list";
 	}
--- src/main/java/com/study/dacord/model/Record.java
+++ src/main/java/com/study/dacord/model/Record.java
@@ -1,10 +1,14 @@
 package com.study.dacord.model;
 
-import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+import lombok.Data;
 
 @Entity
 @Data
@@ -12,6 +16,8 @@ public class Record {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotNull
+	@Size(min=2, max=30)
 	private String title;
 	private String content;
 	
