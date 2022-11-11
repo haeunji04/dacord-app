aa9f503c84714dcd8338b5313ae6686a5f07b6d0
haeunji04 <ppippu12@gmail.com>
2022-11-11 23:47:53 +0900

    feat: api 조건 검색 추가

--- src/main/java/com/study/dacord/controller/RecordApiController.java
+++ src/main/java/com/study/dacord/controller/RecordApiController.java
@@ -3,6 +3,7 @@ package com.study.dacord.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.study.dacord.model.Record;
@@ -22,11 +24,14 @@ class RecordApiController {
 	@Autowired
 	private RecordRepository repository;
 
-  // Aggregate root
-  // tag::get-aggregate-root[]
   @GetMapping("/records")
-  List<Record> all() {
-	  return repository.findAll();
+  List<Record> all(@RequestParam(required = false, defaultValue = "") String title,
+		  		   @RequestParam(required = false, defaultValue = "") String content) {
+	  if(StringUtils.isEmpty(title) && StringUtils.isEmpty(content)) {
+		  return repository.findAll();
+	  } else {
+		  return repository.findByTitleOrContent(title, content);
+	  }
   }
   // end::get-aggregate-root[]
 
--- src/main/java/com/study/dacord/repository/RecordRepository.java
+++ src/main/java/com/study/dacord/repository/RecordRepository.java
@@ -1,8 +1,13 @@
 package com.study.dacord.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.study.dacord.model.Record;
 
 public interface RecordRepository extends JpaRepository<Record, Long>{
 
+	List<Record> findByTitle(String title);
+	List<Record> findByTitleOrContent(String title, String content);
 }
